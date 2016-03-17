aabdf4e0a8214372c9c38d7e3d1ff284f5a599cb
Piski <sergeiliski@gmail.com>
2016-03-17 20:47:16 +0100

    foo

--- js/utils.js
+++ js/utils.js
@@ -12,6 +12,7 @@ var getBooks = function(name) {
     url: "https://www.googleapis.com/books/v1/volumes?q=" + name + "&callback=handleResponse",
     success: function(data) {
         $("#suggestion-list").text(JSON.stringify(data))
+        console.log(data)
     },
     error: function(XMLHttpRequest, textStatus, errorThrown) {
         console.log("Status: " + textStatus); alert("Error: " + errorThrown);
