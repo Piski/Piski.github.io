cd3104b344940f78f60c0db6671908b8127aee6c
Piski <sergeiliski@gmail.com>
2016-03-18 13:06:13 +0100

    foo

--- js/main.js
+++ js/main.js
@@ -11,12 +11,31 @@ function init() {
     }
   });
 
-  $( document ).on( "swipeleft", page, function() {
-    $.mobile.changePage("#pagetwo", { transition: "slide" });
-  });
-  $( document ).on( "swiperight", page, function() {
-    $.mobile.changePage("#pagetwo", { transition: "slide", direction: "reverse" });
-  });
+  $(document).on('swipeleft', '.ui-page', function(event){
+    if(event.handled !== true) // This will prevent event triggering more then once
+    {
+        var nextpage = $.mobile.activePage.next('[data-role="page"]');
+        // swipe using id of next page if exists
+        if (nextpage.length > 0) {
+            $.mobile.changePage(nextpage, {transition: "slide", reverse: false}, true, true);
+        }
+        event.handled = true;
+    }
+    return false;
+});
+
+$(document).on('swiperight', '.ui-page', function(event){
+    if(event.handled !== true) // This will prevent event triggering more then once
+    {
+        var prevpage = $(this).prev('[data-role="page"]');
+        if (prevpage.length > 0) {
+            $.mobile.changePage(prevpage, {transition: "slide", reverse: true}, true, true);
+        }
+        event.handled = true;
+    }
+    return false;
+});
+
 }
 
 (function() {
