f771c20f702713d6b83e8dfaafb357f8e498fb42
Piski <sergeiliski@gmail.com>
2016-03-18 03:17:12 +0100

    foo

--- js/utils.js
+++ js/utils.js
@@ -49,11 +49,13 @@ var populateListView = function(data) {
   data = parseResult(data);
   $("#suggestion-list").html("");
   data.forEach(function(book) {
-    var book = '<li data-icon="plus">' +
-      '<img src="' + book.thumbnail + '">' +
-      '<h2>' + book.title + '</h2>' +
-      '<p>' + book.authors[0] + '</p>' +
-    '</li>';
+    var book = '<a href="#">' +
+      '<li data-icon="plus">' +
+        '<img src="' + book.thumbnail + '">' +
+        '<h2>' + book.title + '</h2>' +
+        '<p>' + book.authors[0] + '</p>' +
+      '</li>' +
+    '</a>';
     $("#suggestion-list").append(book);
   })
 }
