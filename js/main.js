f59a14801f7c2a2eaf3e9cca93d53ea89250786c
Piski <sergeiliski@gmail.com>
2016-03-18 15:30:51 +0100

    foo

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function init() {
   setSwipe();
   setInputListener();
   $("#suggestion-list").on("click", ".save-book", function() {
-    console.log($("#suggestion-list").index(this))
+    console.log($("#suggestion-list li").index(this))
   })
 }
 
