8970c1df503550925abb3e7a0637797d98dd732f
Piski <sergeiliski@gmail.com>
2016-03-22 11:53:44 +0100

    foo

--- js/utils.js
+++ js/utils.js
@@ -214,7 +214,7 @@ var areYouSure = function(text1, text2, button, callback) {
 var setDeleteListener = function() {
   $("#books-list").on("click", ".delete-book", function() {
     var self = this;
-    console.log($(self).parent())
+    console.log($(self).parent().child("h2").text())
     areYouSure("Are you sure?", "", "Delete", function() {
       $(self).parent().hide();
       deleteBook($(self).parent().index());
