e45e39711e35bab046c500b323e1ba03a0bb09a3
Piski <sergeiliski@gmail.com>
2016-03-23 10:57:06 +0100

    foo

--- js/book-finder.js
+++ js/book-finder.js
@@ -1,7 +1,7 @@
 function init() {
   $(document).on("pagebeforeshow","#pageone", function() {
     //$(".pagetwo").removeClass("ui-state-persist");
-    $(".pageone").addClass("ui-state-persist");
+    //$(".pageone").addClass("ui-state-persist");
   });
   setHeader();
   setSwipe();
--- js/book-list.js
+++ js/book-list.js
@@ -3,7 +3,7 @@ function init() {
   setEditListener();
   $(document).on("pagebeforeshow","#pagetwo", function() {
     //$(".pageone").removeClass("ui-state-persist");
-    $(".pagetwo").addClass("ui-state-persist");
+    //$(".pagetwo").addClass("ui-state-persist");
     var books = retrieveBooks();
     populateBookList(books);
     $('#books-list').listview('refresh');
